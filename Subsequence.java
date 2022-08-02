2a0ed5ea9f5448fafb7a7f9cb9a7d0f83752a1b3
Mirudula12 <mirudula12@gmail.com>
2022-08-02 18:32:49 +0530

    LIS

---
+++ Subsequence.java
@@ -0,0 +1,33 @@
+
+public class Subsequence {
+
+		static int lis(int arr[], int n)
+	    {
+	        int lis[] = new int[n];
+	        int i, j, max = 0;
+	 
+	        for (i = 0; i < n; i++)
+	            lis[i] = 1;
+	 
+	        for (i = 1; i < n; i++)
+	            for (j = 0; j < i; j++)
+	                if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+	                    lis[i] = lis[j] + 1;
+	 
+	       
+	        for (i = 0; i < n; i++)
+	            if (max < lis[i])
+	                max = lis[i];
+	 
+	        return max;
+	    }
+		public static void main(String args[]) {
+		
+			int arr[] = {13,1,3,4,8,19,17,8,0,20,14};
+			int n = arr.length;
+			System.out.println("Length of LIS: " + lis(arr, n));
+		}
+	
+	
+
+}
